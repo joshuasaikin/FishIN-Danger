e02edcbea862310468ee33c2b59b63210d240dd7
electricjosh88 <>
2021-04-20 20:04:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,88 @@
+var myGamePiece;
+var myObstacle;
+// This fucntion is definign the length and width of the canvas.
+
+// function myCanvas(){
+// var c=document.getElementById("timmy");
+// var ctx=c.getContext("2d");
+// var img=document.getElementById("bottle1");
+// ctx.drawImage(img,10,10,300,150);
+// var img1=document.getElementById("boat1");
+// ctx.drawImage(img1,10,10,300,150);
+// }
+// myCanvas();
+// function collideMouse() {
+
+  // document.getElementsByTagName("canvas");
+  // document.canvas.width=680;
+  // document.canvas.height=360;
+
+
+//   this.canvas.width = 680;
+//   this.canvas.height = 360;
+//   this.context = this.canvas.getContext("bottle");
+
+//   HTMLCanvasElement.prototype.relMouseCoords = function (event) {
+//     var totalOffsetX = 0;
+//     var totalOffsetY = 0;
+//     var canvasX = 0;
+//     var canvasY = 0;
+//     var currentElement = this;
+
+//     do {
+//       totalOffsetX += currentElement.offsetLeft;
+//       totalOffsetY += currentElement.offsetTop;
+//     }
+//     while (currentElement = currentElement.offsetParent)
+
+//     canvasX = event.pageX - totalOffsetX;
+//     canvasY = event.pageY - totalOffsetY;
+
+//     // Fix for variable canvas width
+//     canvasX = Math.round(canvasX * (this.width / this.offsetWidth));
+//     canvasY = Math.round(canvasY * (this.height / this.offsetHeight));
+
+//     return { x: canvasX, y: canvasY }
+
+
+//     console.log({ x: canvasX, y: canvasY });
+//   }
+
+// }
+// collideMouse();
+//   /* 
+//   function collidetrash() {
+//     this.canvas.width = 680;
+//     this.canvas.height = 720;
+//     this.context = this.canvas.getContext("bottle");
+
+//     HTMLCanvasElement.prototype.relTrashCoords = function (event) {
+//       var totalOffsetX = 0;
+//       var totalOffsetY = 0;
+//       var canvasX = 0;
+//       var canvasY = 0;
+//       var currentElement = this;
+
+//       do {
+//         totalOffsetX += currentElement.offsetLeft;
+//         totalOffsetY += currentElement.offsetTop;
+//       }
+//       while (currentElement = currentElement.offsetParent)
+
+//       canvasX = event.pageX - totalOffsetX;
+//       canvasY = event.pageY - totalOffsetY;
+
+//       // Fix for variable canvas width
+//       canvasX = Math.round(canvasX * (this.width / this.offsetWidth));
+//       canvasY = Math.round(canvasY * (this.height / this.offsetHeight));
+
+//       return { x: canvasX, y: canvasY }
+
+//       if (x ==)
+
+//         console.log("you working bro");
+//     }
+//     collidetrash();
+//   }
+//  */
+
